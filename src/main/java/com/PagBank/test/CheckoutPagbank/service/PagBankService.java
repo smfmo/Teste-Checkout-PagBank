f9d2a08fd81d582962b4192b7843c3494adabdf1
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-05-21 09:40:23 -0300

    melhorias no código

--- src/main/java/com/PagBank/test/CheckoutPagbank/service/PagBankService.java
+++ src/main/java/com/PagBank/test/CheckoutPagbank/service/PagBankService.java
@@ -1,40 +1,40 @@
 package com.PagBank.test.CheckoutPagbank.service;
 
 import com.PagBank.test.CheckoutPagbank.model.Checkout;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class PagBankService {
 
     private final RestTemplate restTemplate;
 
-    // Token fornecido pelo sandbox do PagBank
-    private static final String BEARER_TOKEN = "92de68c1-d123-42fa-be33-0d60def3958390c36cdc4a10956174a45186c40c0fd44a52-f543-43a7-8c65-edec58360c4a";
+    @Value("${pagBank.api.bearer_token}")
+    private String bearerToken;
 
-    // URL da API
-    private static final String PAGBANK_URL = "https://sandbox.api.pagseguro.com/checkouts";
-
-    public PagBankService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    @Value("${pagBank.api.url}")
+    private String pagBankUrl;
 
     public ResponseEntity<String> criarCheckout(Checkout checkout) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(BEARER_TOKEN);
+        headers.setBearerAuth(bearerToken);
 
         HttpEntity<Checkout> request = new HttpEntity<>(checkout, headers);
 
-        return restTemplate.postForEntity(PAGBANK_URL, request, String.class);
+
+        return restTemplate.postForEntity(pagBankUrl, request, String.class);
     }
 
     public ResponseEntity<String> getCheckoutById(String id){
         HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(BEARER_TOKEN);
+        headers.setBearerAuth(bearerToken);
         RequestEntity<Void> requestEntity = RequestEntity
-                .get(PAGBANK_URL + "/" + id)
+                .get(pagBankUrl + "/" + id)
                 .headers(headers)
                 .accept(MediaType.APPLICATION_JSON)
                 .build();
