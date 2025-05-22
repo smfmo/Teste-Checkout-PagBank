cb81a5ad72e886109b9f88bb6031ebc1a5e5d7f1
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-05-22 11:12:50 -0300

    melhorias na segurança

--- src/main/java/com/PagBank/test/CheckoutPagbank/repository/CheckoutRepository.java
+++ src/main/java/com/PagBank/test/CheckoutPagbank/repository/CheckoutRepository.java
@@ -4,5 +4,4 @@ import com.PagBank.test.CheckoutPagbank.model.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CheckoutRepository extends JpaRepository<Checkout, String> {
-
 }
